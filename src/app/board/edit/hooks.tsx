a412bb48b24c34045ca259a88c89b6f2abf173be
gilgor2 <misby0327@gmail.com>
2024-06-01 20:05:52 +0900

    feat(page hook): hooks for affirmation edit page, board edit page

---
+++ src/app/board/edit/hooks.tsx
@@ -0,0 +1,161 @@
+import { CategoryObj } from '@/type/board';
+import { MAX_IMAGE_PER_CATEGORY } from '@/utility/constants';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export const useBoardEditPageState = () => {
+    const [boardData, setboardData] = useState<CategoryObj[]>([]);
+    const [isPopup, setisPopup] = useState(false);
+    const [targetCategory, settargetCategory] = useState('');
+
+    useEffect(() => {
+        const getBoardData = async () => {
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board`);
+            return response.data || [];
+        };
+        const setBoardDataState = async () => {
+            const dataFromDB = await getBoardData();
+            setboardData(dataFromDB);
+        };
+        setBoardDataState();
+    }, []);
+
+    const openPopup = (category:string) => {
+        setisPopup(true);
+        settargetCategory(category);
+    };
+    const closePopup = () => {
+        setisPopup(false);
+        settargetCategory('');
+    };
+
+    const insertImageFromDB = async (src:string) => {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board`, {
+ data: {
+            img_src: src,
+            tag: targetCategory,
+        },
+});
+    };
+
+    const deleteImageFromDB = async (src:string) => {
+        await axios.delete(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board`, { data: { src } });
+    };
+    const deleteCategoryFromDB = async (category:string) => {
+        await axios.delete(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/category`, { data: { category } });
+    };
+    const editCategoryFromDB = async (originalCategory:string, newCategory:string) => {
+        await axios.put(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/category`, { data: { originalCategory, newCategory } });
+    };
+    const insertFileToStorage = async (file:File) => {
+        const formData = new FormData();
+        formData.append('file', file);
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/storage`, formData);
+        const src = response.data.response?.data?.fullPath;
+        return `${process.env.NEXT_PUBLIC_SUPABASE_STORAGE_ENDPOINT}/${src}`;
+    };
+    const addImageTo = (newSrc:string) => {
+        const categoryIndex = boardData.findIndex((obj) => obj.name === targetCategory);
+        if (categoryIndex > -1) {
+            const { srcArr } = boardData[categoryIndex];
+            if (srcArr.length < MAX_IMAGE_PER_CATEGORY) {
+                setboardData((state) => {
+                    const tmp = structuredClone(state);
+                    tmp[categoryIndex].srcArr = [...srcArr, newSrc];
+                    return tmp;
+                });
+            }
+            insertImageFromDB(newSrc);
+        }
+    };
+    const addFileTo = async (file:string | File) => {
+        if (typeof file !== 'string') {
+            const src = await insertFileToStorage(file);
+            return src;
+        }
+        return '-1';
+    };
+    const dropImageTo = async (file:string | File) => {
+        const src = await addFileTo(file);
+        if (src === '-1') {
+            return -1;
+        }
+        addImageTo(src);
+        return 0;
+    };
+    const deleteImageFrom = (src:string) => {
+            setboardData((state) => {
+                const categoryIndex = state.findIndex((obj) => obj.name === targetCategory);
+                if (categoryIndex > -1) {
+                    const tmp = structuredClone(state);
+                    const category = tmp[categoryIndex];
+                    const tmpSrcArr = [...category.srcArr];
+                    const targetIndex = tmpSrcArr.findIndex((str) => str === src);
+                    if (targetIndex > -1) {
+                        tmpSrcArr.splice(targetIndex, 1);
+                    }
+                    category.srcArr = tmpSrcArr;
+                    return tmp;
+                }
+
+                return state;
+            });
+            deleteImageFromDB(src);
+    };
+    const editCategory = (text:string) => {
+        setboardData((state) => {
+            const tmp = structuredClone(state);
+            const originalCategory = tmp.find((obj) => obj.name === targetCategory);
+            if (originalCategory) {
+                originalCategory.name = text;
+            }
+            return tmp;
+        });
+        settargetCategory(text);
+        if (text) {
+            editCategoryFromDB(targetCategory, text);
+        }
+    };
+
+    const deleteCategory = (category:string) => {
+        setboardData((state) => {
+            const tmp = [...state];
+            const delIndex = tmp.findIndex((obj) => obj.name === category);
+            if (delIndex > -1) {
+                tmp.splice(delIndex, 1);
+
+                return tmp;
+            }
+            return state;
+        });
+        setisPopup(false);
+        settargetCategory('');
+        deleteCategoryFromDB(category);
+    };
+    const addNewCategory = () => {
+        setboardData((state) => {
+            const tmp = [...state];
+            const newObj:CategoryObj = { name: 'new!', srcArr: [] };
+            tmp.push(newObj);
+
+            return tmp;
+        });
+        settargetCategory('new!');
+        setisPopup(true);
+    };
+    return {
+        boardData,
+        targetCategory,
+        isPopup,
+        openPopup,
+        closePopup,
+
+        addImageTo,
+        dropImageTo,
+        deleteImageFrom,
+        deleteCategory,
+        editCategory,
+        addNewCategory,
+
+    };
+};
