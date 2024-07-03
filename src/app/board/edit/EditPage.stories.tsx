4e5a97adbd980ca780076b5c0437faf243212720
gilgor2 <misby0327@gmail.com>
2024-07-03 20:39:51 +0900

    test(board) : add test to board
    - 책임 명시가 필요한 컴포넌트에 테스트 추가
    - storybook에 module mock을 추가해 서버 액션 모킹

--- src/app/board/edit/EditPage.stories.tsx
+++ src/app/board/edit/EditPage.stories.tsx
@@ -1,11 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { getByDisplayValue, userEvent, within } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 import { http, HttpResponse } from 'msw';
+import { createMock } from 'storybook-addon-module-mock';
 
 import { DUMMY_BOARD_DATA, DUMMY_NAVER_IMAGE_LIST } from '@/utility/constants';
 import { sleep } from '@/utility/utility';
 import EditPage from './page';
+import * as boardAction from '../../../action/boardAction';
 
 const meta: Meta<typeof EditPage> = {
   title: 'board/edit/EditPage.stories.tsx',
@@ -16,40 +18,24 @@ const meta: Meta<typeof EditPage> = {
 export default meta;
 type Story = StoryObj<typeof EditPage>;
 
-const safeHandlers = [
-  http.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board`, () => HttpResponse.json({})),
-  http.delete(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board`, () => HttpResponse.json({})),
-  http.delete(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/category`, () =>
-    HttpResponse.json({}),
-  ),
-  http.put(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/category`, () => HttpResponse.json({})),
-  http.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/storage`, () => HttpResponse.json({})),
-  http.get(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/naver-image?searchText=카리나`, () =>
-    HttpResponse.json(DUMMY_NAVER_IMAGE_LIST),
-  ),
-];
+const naverHandler = http.get(
+  `${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/naver-image?searchText=카리나`,
+  () => HttpResponse.json(DUMMY_NAVER_IMAGE_LIST),
+);
+
+const getBoardDataMock = () => {
+  const mock = createMock(boardAction, 'getBoardData');
+  mock.mockReturnValue(Promise.resolve(DUMMY_BOARD_DATA));
+  return [mock];
+};
 
 export const Default: Story = {
-  parameters: {
-    msw: {
-      handlers: [
-        http.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/edit`, () =>
-          HttpResponse.json(DUMMY_BOARD_DATA),
-        ),
-        ...safeHandlers,
-      ],
-    },
-  },
+  parameters: {},
 };
 export const AddCategory: Story = {
   parameters: {
     msw: {
-      handlers: [
-        http.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/edit`, () =>
-          HttpResponse.json([]),
-        ),
-        ...safeHandlers,
-      ],
+      handlers: [naverHandler],
     },
   },
   play: async ({ canvasElement }) => {
@@ -76,73 +62,58 @@ export const AddCategory: Story = {
     expect(previewImage?.srcset).toBe(firstImage.srcset);
   },
 };
-// export const EditCategory: Story = {
-//   parameters: {
-//     msw: {
-//       handlers: [
-//         http.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/edit`, () =>
-//           HttpResponse.json(DUMMY_BOARD_DATA),
-//         ),
-//         // ...safeHandlers,
-//       ],
-//     },
-//   },
-//   play: async ({ canvasElement }) => {
-//     const canvas = within(canvasElement);
-//     await sleep(1000);
+export const EditCategory: Story = {
+  parameters: {
+    moduleMock: {
+      mock: getBoardDataMock,
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await sleep(1000);
 
-//     await userEvent.click(canvas.getAllByText('edit')[0]);
+    await userEvent.click(canvas.getAllByText('edit')[0]);
 
-//     await userEvent.type(canvas.getAllByDisplayValue('운동')[0], '끝');
+    await userEvent.type(canvas.getAllByDisplayValue('운동')[0], '끝');
 
-//     await userEvent.click(canvas.getByText('x'));
-//     await sleep(1000);
+    await userEvent.click(canvas.getByText('x'));
+    await sleep(1000);
 
-//     expect(canvas.getByText('운동끝')).toBeInTheDocument();
-//   },
-// };
-// export const DeleteCategory: Story = {
-//   parameters: {
-//     msw: {
-//       handlers: [
-//         http.post(`${process.env.NEXT_PUBLIC_ROUTER_URL}/board/edit`, () =>
-//           HttpResponse.json(DUMMY_BOARD_DATA),
-//         ),
-//         ...safeHandlers,
-//       ],
-//     },
-//   },
-//   play: async ({ canvasElement }) => {
-//     const canvas = within(canvasElement);
-//     await sleep(1000);
+    expect(canvas.getByText('운동끝')).toBeInTheDocument();
+  },
+};
+export const DeleteCategory: Story = {
+  parameters: {
+    moduleMock: {
+      mock: getBoardDataMock,
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await sleep(1000);
 
-//     await userEvent.click(canvas.getAllByText('edit')[0]);
+    await userEvent.click(canvas.getAllByText('edit')[0]);
 
-//     await userEvent.click(canvas.getByText('"운동" 삭제하기'));
+    await userEvent.click(canvas.getByText('"운동" 삭제하기'));
 
-//     expect(canvas.queryByText('운동')).toBeNull();
-//   },
-// };
-// export const DeleteImage: Story = {
-//   parameters: {
-//     msw: {
-//       handlers: [
-//         http.post(`${process.env.NEXT_PUBLIC_API_ROUTER_URL}/board/edit`, () =>
-//           HttpResponse.json(DUMMY_BOARD_DATA),
-//         ),
-//         ...safeHandlers,
-//       ],
-//     },
-//   },
-//   play: async ({ canvasElement }) => {
-//     const canvas = within(canvasElement);
-//     await sleep(1000);
+    expect(canvas.queryByText('운동')).toBeNull();
+  },
+};
+export const DeleteImage: Story = {
+  parameters: {
+    moduleMock: {
+      mock: getBoardDataMock,
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await sleep(1000);
 
-//     await userEvent.click(canvas.getAllByText('edit')[0]);
+    await userEvent.click(canvas.getAllByText('edit')[0]);
 
-//     const firstDeleteButton = canvas.getAllByText('delete')[0];
+    const firstDeleteButton = canvas.getAllByText('delete')[0];
 
-//     await userEvent.click(firstDeleteButton);
-//     expect(canvas.getAllByText('delete').length).toBe(5);
-//   },
-// };
+    await userEvent.click(firstDeleteButton);
+    expect(canvas.getAllByText('delete').length).toBe(5);
+  },
+};
